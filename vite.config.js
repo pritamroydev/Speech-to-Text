03011fdfca3ec73df57e4913f44e0d9c0d990945
Pritam Roy <lightyagami180404@gmail.com>
2026-07-19 18:58:28 +0530

    Fix PWA icons

--- vite.config.js
+++ vite.config.js
@@ -22,17 +22,17 @@ export default defineConfig({
         scope: '/',
         icons: [
           {
-            src: '/pwa-192.png',
+            src: '/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/pwa-512.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/pwa-512-maskable.png',
+            src: '/pwa-512x512-maskable.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable',
